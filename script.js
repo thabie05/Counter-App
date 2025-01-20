6098f999b69debc3deff8468927d1fa05dc4e9c6
thabie05 <105032791+thabie05@users.noreply.github.com>
2025-01-20 20:48:09 +0200

    added the total

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let count = 0;
+let total = 0;
 let counter = document.getElementById('count-el');
 let saveEl = document.getElementById('save-el');
+let totalNmbr = document.getElementById('total-nmbr')
 
  function increment() {
     count += 1;
@@ -9,7 +11,11 @@ let saveEl = document.getElementById('save-el');
 
  function save() {
     let both = count + " - ";
+    saveEl.textContent += both;
+
+    total += count;
+    totalNmbr.textContent = total;
+
     count = 0;
     counter.textContent = 0;
-    saveEl.textContent += both;
  }
